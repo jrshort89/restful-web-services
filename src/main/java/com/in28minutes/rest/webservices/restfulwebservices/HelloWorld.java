6bab6c08c55ca642efa9f26b3f937c7cf48e4f42
Jake Short <jrshort89@gmail.com>
2022-01-06 16:02:47 -0500

    initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+public class HelloWorld {
+    private String body;
+
+    public HelloWorld(String body){
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("HelloWorldBean [body=%s]", body);
+    }
+}
